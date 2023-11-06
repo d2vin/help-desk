f7141084ef8d1be49b2365e55be3923eddf1e997
Devin Minnihan <devinminnihan@gmail.com>
2023-11-06 14:33:21 -0500

    fix auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,7 @@
-// import NextAuth from "next-auth";
+import NextAuth from "next-auth";
 
-// import { authOptions } from "~/server/auth";
+import { authOptions } from "~/server/auth";
 
-// // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-// const handler = NextAuth(authOptions);
-// export { handler as GET, handler as POST };
-
-import Nextauth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-
-export default Nextauth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-});
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
